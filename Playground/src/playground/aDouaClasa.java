e2f3322dc036ccd13213cf8947c66593e5bad481
Florin <Florin@DESKTOP-ADGURQB>
2020-03-20 14:51:24 +0200

    asd

--- Playground/src/playground/aDouaClasa.java
+++ Playground/src/playground/aDouaClasa.java
@@ -1,6 +1,7 @@
 package playground;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class aDouaClasa {
@@ -17,22 +18,27 @@ public class aDouaClasa {
 				System.out.println ("class:2 // method:1");
 			}
 	
+	@Parameters({"URL"}) //Picks up the parameter defined in the TestNG XML file 
 	@Test(timeOut=4000)
-		public void class2method2() 
+		public void class2method2(String urlname) // Whatever is stored in the "URL" will go into urlname and can be printed in the output (gets the data from the xml file 
 			{
 				System.out.println("class2: // method:2");	
+				System.out.println(urlname);
 			}
 	
 	@Test(dependsOnMethods= {"class2method4", "class2method2"})
+	//Depends on other methods - runs after the other methods are finished running
 		public void class2method3() 
 			{
 				System.out.println("class2: // method:3");	
 			}
 	
+	@Parameters("RLU") // This parameter is only set-up for this tests and can only be used in theis class (check testng.xml for further details on initialization of the parameter)
 	@Test(enabled=true)
-		public void class2method4()
+		public void class2method4(String lruName)
 			{
 				System.out.println("class2: // method:4");	
+				System.out.println(lruName);
 			}
 	
 	@AfterTest
--- Playground/src/playground/primaClasa.java
+++ Playground/src/playground/primaClasa.java
@@ -9,30 +9,35 @@ import org.testng.annotations.Test;
 
 public class primaClasa {
 	
-	@BeforeSuite
+	@BeforeSuite 
+	//Runs before running the test suite 
 	public void BFsuite ()
 	{
 	System.out.println("I am the 1st thing that is being run in here!");
 	}
 	
-	@BeforeTest
+	@BeforeTest 
+	//Runs before running the test 
 	public void MethodBefore()
 	{
 	System.out.println("class1: // method before test rulata");
 	}
 	
-	@BeforeMethod
+	@BeforeMethod 
+	//Runs before running the methods
 	public void Before()
 	{
 	System.out.println("class1: // I am before method, crazy method!");
 	}
 	
 	@Test(groups= {"smoke"})
+	//Makes the test as being part of a group - from xml, you can run only tests being part of a group - i.e. smoke, regression, sanity etc 
 	public void class1method1()
 	{
 	System.out.println ("class:1 // method:1");
 	}
 	
+	@Test
 	public void class1method2() 
 	{
 	System.out.println("class1: // method:2");	
@@ -49,19 +54,22 @@ public class primaClasa {
 	System.out.println("class1: // method:4");	
 	}
 	
-	@AfterMethod
+	@AfterMethod 
+	//Runs after the method is ran
 	public void After()
 	{
 	System.out.println("class1: // I am after method, craziest method!!!");
 	}
 	
 	@AfterTest
+	//Runs after the test is ran (the last one to run)
 	public void methodAfter()
 	{
 	System.out.println("class1: // method after test rulata");	
 	}
 	
 	@AfterSuite
+	//Runs after the suite is run
 	public void AFsuite ()
 	{
 		System.out.println("I am the last one in the chain!!!");
